ca1862451a9c2e5a1048dd3c50f0fcfc1a7e9ed6
Nabeerah Yaqoot <nabeerahyaqoot@gmail.com>
2025-08-01 08:33:24 +0500

    Update Main class

--- src/Week_01/ClassCode/Main.java
+++ src/Week_01/ClassCode/Main.java
@@ -8,9 +8,23 @@ public class Main {
         Student s1 = new Student("Amna", 20, 7);
         s1.printInfo();
 
+        System.out.println("-------------------------------");
+
         // Creating second Student object
         Student s2 = new Student("Ali", 25, 17);
         s2.printInfo();
+
+        System.out.println("-------------------------------");
+
+        // Creating first Vehicle object
+        Vehicle myCar = new Vehicle("Green", "Honda", 2023);
+        myCar.printDetails();
+
+        System.out.println("-------------------------------");
+
+        // Creating second Vehicle object
+        Vehicle myBus = new Vehicle("Blue", "Daewoo Express", 2015);
+        myBus.printDetails();
 
     }
 
