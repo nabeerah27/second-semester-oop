f975c2c7ee7711fafd7cfdad50b8ff77e5671436
Nabeerah Yaqoot <nabeerahyaqoot@gmail.com>
2025-08-11 22:10:39 +0500

    Make object of toy program

--- src/Week_04/LabTask/Main.java
+++ src/Week_04/LabTask/Main.java
@@ -1,28 +1,13 @@
 package Week_04.LabTask;
 
-import Week_04.LabTask.derived_classes.BankManager;
-import Week_04.LabTask.derived_classes.Clown;
-import Week_04.LabTask.derived_classes.EngineDriver;
-import Week_04.LabTask.derived_classes.Gardener;
+import Week_04.LabTask.base_class.ToyProgram;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        EngineDriver engineDriver = new EngineDriver("Large");
-        System.out.println(engineDriver);
-        System.out.println("-----------------------------------");
-
-        Gardener gardener = new Gardener("Small");
-        System.out.println(gardener);
-        System.out.println("-----------------------------------");
-
-        BankManager bankManager = new BankManager("Medium");
-        System.out.println(bankManager);
-        System.out.println("-----------------------------------");
-
-        Clown clown = new Clown("Small");
-        System.out.println(clown);
+        ToyProgram toyProgram = new ToyProgram();
+        toyProgram.run();
 
     }
 }
