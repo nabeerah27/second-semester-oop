13ce5e21dc4a71676217f7ae6a6e79bbe6cc5dde
Nabeerah Yaqoot <nabeerahyaqoot@gmail.com>
2025-08-11 22:46:41 +0500

    Update main method to improve output formatting

--- src/Week_02/HomeWork/Main.java
+++
@@ -1,4 +0,0 @@
-package Week_02.HomeWork;
-
-public class Main {
-}
---
+++ src/Week_02_Point_Pen/ClassCode/Main.java
@@ -0,0 +1,23 @@
+package Week_02_Point_Pen.ClassCode;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Point2D point = new Point2D(3, 5);
+        System.out.println(point);
+
+        System.out.println("--------------------------------");
+
+        Point3D point3D = new Point3D(4, 5, 7);
+        System.out.println(point3D);
+
+        System.out.println("--------------------------------");
+
+        Pen picasso = new Pen(point, 5.7, "Green", "Ball point", true);
+        System.out.println(picasso);
+
+
+    }
+
+}
