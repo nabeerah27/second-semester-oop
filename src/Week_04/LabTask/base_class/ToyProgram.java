88a1c5bd3416bfca8e7a6b88633d00fda95c971d
Nabeerah Yaqoot <nabeerahyaqoot@gmail.com>
2025-08-11 22:08:26 +0500

    Added toy program class

---
+++ src/Week_04/LabTask/base_class/ToyProgram.java
@@ -0,0 +1,28 @@
+package Week_04.LabTask.base_class;
+
+import Week_04.LabTask.derived_classes.BankManager;
+import Week_04.LabTask.derived_classes.Clown;
+import Week_04.LabTask.derived_classes.EngineDriver;
+import Week_04.LabTask.derived_classes.Gardener;
+
+public class ToyProgram {
+    public void run() {
+
+        EngineDriver engineDriver = new EngineDriver("Large");
+        System.out.println(engineDriver);
+        System.out.println("-----------------------------------");
+
+        Gardener gardener = new Gardener("Small");
+        System.out.println(gardener);
+        System.out.println("-----------------------------------");
+
+        BankManager bankManager = new BankManager("Medium");
+        System.out.println(bankManager);
+        System.out.println("-----------------------------------");
+
+        Clown clown = new Clown("Small");
+        System.out.println(clown);
+
+    }
+
+}
